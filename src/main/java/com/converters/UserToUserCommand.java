9a92cb17037acca7f54c13bf289d29f2c1e78041
jannkasper <jann.kasper@gmail.com>
2021-04-22 07:26:35 +0200

    mainPage | display sidecars + videos

--- src/main/java/com/converters/UserToUserCommand.java
+++ src/main/java/com/converters/UserToUserCommand.java
@@ -26,9 +26,9 @@ public class UserToUserCommand implements Converter<User, UserCommand> {
             userCommand.setPostArray(new ArrayList<Node__PostCommand>());
             Node__PostToNode__PostCommand nodeConverter = new Node__PostToNode__PostCommand();
             for (Edge__Post edge__post : source.getEdge_web_feed_timeline().getEdges()) {
-                if (edge__post.getNode().get__typename().equals("GraphImage")) {
-//                        || edge__post.getNode().get__typename().equals("GraphVideo")
-//                        || edge__post.getNode().get__typename().equals("GraphSidecar") ) {
+                if (edge__post.getNode().get__typename().equals("GraphImage")
+                        || edge__post.getNode().get__typename().equals("GraphVideo")
+                        || edge__post.getNode().get__typename().equals("GraphSidecar") ) {
                     final Node__PostCommand node = nodeConverter.convert(edge__post.getNode());
                     userCommand.getPostArray().add(node);
                 }
